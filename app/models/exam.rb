6bd550ee78015a4173424776c47150853dc67322
Misha <shogun.pvl@gmail.com>
2022-01-22 02:58:59 +0700

    add validation

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,14 +1,12 @@
 class Exam < ApplicationRecord
-  validates :first_name, length: { maximum: 40 }, presence: true
-  validates :middle_name, length: { maximum: 60 }
-  validates :last_name, length: { maximum: 40 }, presence: true
+  validates :first_name, length: { in: 3..20 }, presence: true
+  validates :middle_name, length: { in: 3..20 }
+  validates :last_name, length: { in: 3..20 }, presence: true
   validates :subject, presence: true
-  validate :happened_at_is_valid_datetime
+  validates :date, timeliness: { on_or_after: lambda { Date.current }, type: :date }
 
 
-  def happened_at_is_valid_datetime
-    errors.add(:Ошибка, 'введите дату формата YYYY-MM-DD') if ((DateTime.parse(date) rescue ArgumentError) == ArgumentError)
-  end
+
 
   def full_name
     if middle_name.present?
