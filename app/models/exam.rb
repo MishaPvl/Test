68affe9fc9d23616ac3e695ade8ba8d24ebae8e2
Misha <shogun.pvl@gmail.com>
2022-01-22 00:48:33 +0700

    change db migrate

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -3,8 +3,12 @@ class Exam < ApplicationRecord
   validates :middle_name, length: { maximum: 60 }
   validates :last_name, length: { maximum: 40 }, presence: true
   validates :subject, presence: true
+  validate :happened_at_is_valid_datetime
 
 
+  def happened_at_is_valid_datetime
+    errors.add(:Ошибка, 'введите дату формата YYYY-MM-DD') if ((DateTime.parse(date) rescue ArgumentError) == ArgumentError)
+  end
 
   def full_name
     if middle_name.present?
